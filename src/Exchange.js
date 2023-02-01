d369e5a254e56cf96d1696c8aa140f036ca6db22
Wale Osho <90667947+wbtech@users.noreply.github.com>
2023-02-02 00:58:19 +0100

    Update Exchange.js

---
+++ src/Exchange.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const CurrencyConverter = () => {
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('GBP');
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [exchangeRates, setExchangeRates] = useState({});
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleFromCurrencyChange = (e) => {
+    setFromCurrency(e.target.value);
+  };
+
+  const handleToCurrencyChange = (e) => {
+    setToCurrency(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`);
+    const result = await response.json();
+    setExchangeRates(result.rates);
+    setConvertedAmount((amount * result.rates[toCurrency]).toFixed(2));
+  };
+
+  return (
+    <div className="currency-converter">
+      <h1>Currency Converter</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="number" value={amount} onChange={handleAmountChange} />
+        <select value={fromCurrency} onChange={handleFromCurrencyChange}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+        </select>
+        <select value={toCurrency} onChange={handleToCurrencyChange}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+        </select>
+        <button type="submit">Convert</button>
+      </form>
+      <h2>{convertedAmount}</h2>
+    </div>
+  );
+};
+
+export default CurrencyConverter;
